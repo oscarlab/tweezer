ec6804dd8a30dd1e948af8a7554e961d89a890ea
Don Porter <porter@cs.unc.edu>
2025-02-14 15:04:34 -0500

    Copy out the final binary

--- script/compile.sh
+++ script/compile.sh
@@ -7,3 +7,4 @@ docker run $MOUNT_SGXDEVICE --name tweezer tweezer:0.0 /root/compile.sh
 mkdir -p binary
 docker cp tweezer:/root/tweezer/db_bench binary/tweezer
 docker cp tweezer:/root/speicher_V1/db_bench binary/speicher
+docker cp tweezer:/root/tweezer/ycsbc_bin binary/tweezer-ycsb
